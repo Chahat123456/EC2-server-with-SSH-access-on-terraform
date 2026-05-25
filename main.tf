d7626d8f9bada529a9af7cb28bdefb13d03afaa5
Chahat123456 <143415219+Chahat123456@users.noreply.github.com>
2026-05-25 21:08:34 +0545

    Done in apa-south-1

---
+++ main.tf
@@ -0,0 +1,67 @@
+# Default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Default Subnet (pick first one)
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["27.34.66.41/32"]  #cidr_blocks = ["YOUR_IP/32"] You need to use your ip through this command "curl -4 ifconfig.me"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance (MISSING BEFORE — THIS WAS THE BIG ISSUE)
+resource "aws_instance" "ec2" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type              = var.instance_type
+  subnet_id                  = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids     = [aws_security_group.web_sg.id]
+
+  key_name = "id_ed25519"
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.instance_name
+  }
+}
